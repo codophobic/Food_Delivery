563664275a5714e98d8dab33b236e888754111c0
ANKIT <mr.ankityo111@gmail.com>
2020-07-30 18:36:54 +0530

    port change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const resRoutes = require('./Routes/restaurant');
 const adminRoutes = require('./Routes/admin');
 const cors= require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
